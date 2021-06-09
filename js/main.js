e9b810b45f07643e3937397569e5689aa3d5f93d
tushars000 <tushar.solanki4@gmail.com>
2021-06-09 17:16:35 +0530

    Changint he layout of Team page and adding new team images

--- js/main.js
+++ js/main.js
@@ -359,8 +359,6 @@ mailChimp();
       document.addEventListener('mousemove', function(e){
       var x = e.clientX;
       var y = e.clientY;
-      cursor.style.left = x + 'px';
-      cursor.style.top = y + 'px';
       })
 
 })(jQuery);	
